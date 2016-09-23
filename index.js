4450296f8cbd8977336cd732ab2a39dc0a30da5e
Magnus Morris <magnusmorris@icloud.com>
2016-09-23 22:34:35 +1000

    babel transpile ammends

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
+import 'babel-polyfill'
 import ReactGithubStars from './src/ReactGithubStars'
 export default ReactGithubStars
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 module.exports = [
   {
-    entry: ['babel-polyfill', 'whatwg-fetch', './index.js'],
+    entry: './index.js',
     output: {
       path: './dist/',
       filename: 'bundle.js',
