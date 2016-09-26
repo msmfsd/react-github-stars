5a5b62996eb16cbd6a1807714d36ec185d60dfb6
Magnus Morris <magnusmorris@icloud.com>
2016-09-26 16:54:53 +1000

    Updated build scripts

--- index.js
+++
@@ -1,3 +0,0 @@
-import 'babel-polyfill'
-import ReactGithubStars from './src/ReactGithubStars'
-export default ReactGithubStars
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,5 @@
 module.exports = [
   {
-    entry: './index.js',
-    output: {
-      path: './dist/',
-      filename: 'bundle.js',
-    },
-    module: {
-      loaders: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: 'babel',
-          query: {
-            presets: ['es2015', 'stage-0', 'react'],
-            plugins: ['add-module-exports']
-          }
-        },
-        {
-          test: /\.css$/,
-          loader: 'style-loader!css-loader!postcss-loader'
-        }
-      ]
-    },
-    postcss: () => {
-      return [
-        require('postcss-sorting'),
-        require('postcss-cssnext'),
-        require('postcss-extend')
-      ]
-    }
-  },
-  {
     entry: './examples/app.js',
     output: {
       path: './examples/',
